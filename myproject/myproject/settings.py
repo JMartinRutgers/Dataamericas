73250cf71ff693c505e0b26ca4d89551a9fe1180
Jorge Melchor <48293166+JMartinRutgers@users.noreply.github.com>
2024-10-27 14:32:19 -0400

    Add files via upload

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -119,13 +119,18 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+#https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 #STATIC_ROOT = 'staticfiles'
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#MEDIA_URL = '/images/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static',)]
+
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '\static',)]
 
 
 
