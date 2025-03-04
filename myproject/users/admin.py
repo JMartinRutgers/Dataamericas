00377ae5cb4c73f32cc5134d3722a7218f734eb0
Jorge Melchor <48293166+JMartinRutgers@users.noreply.github.com>
2025-03-03 23:30:10 -0500

    Add files via upload

--- myproject/users/admin.py
+++ myproject/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    # Add any custom fields to these lists
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    fieldsets = UserAdmin.fieldsets
+    # If you have custom fields, add them like this:
+    # fieldsets = UserAdmin.fieldsets + (
+    #     ('Custom Fields', {'fields': ('custom_field1', 'custom_field2')}),
+    # )
+
+# Register with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
